ad75e31bb624165e0d03742bea1790f339f75f11
princebhardwaj9 <princebhardwaj.110@gmail.com>
2020-04-24 01:31:46 +0530

    FSJ0320A021

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {HomeComponent} from './home/home.component';
+import {DatabindingComponent} from './databinding/databinding.component';
+import {AboutComponent} from './about/about.component';
+
+
+const routes: Routes = [
+  {path: '' , component: HomeComponent},
+  {path: 'databinding' , component: DatabindingComponent},
+  {path: 'about' , component: AboutComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
